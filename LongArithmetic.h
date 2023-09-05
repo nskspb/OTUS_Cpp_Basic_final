4e0376a36f52b8b74ddfc02b827c5559c5b58d13
Ruslan <nugaev.ruslan2010@yandex.ru>
2023-09-05 20:10:30 +0300

    mul and del

--- LongArithmetic.h
+++ LongArithmetic.h
@@ -4,73 +4,79 @@
 #include <vector>
 #include <algorithm>
 
-
-class LongNum {
-    private: 
-    std::vector <int> number;
+class LongNum
+{
+private:
+    std::vector<int> number;
     bool sign;
 
-    public:
-
+public:
     LongNum();
-    LongNum(std::vector<int>& vec, bool sig);
-    LongNum( std::string & str);
+    LongNum(std::vector<int> &vec, bool sig);
+    LongNum(std::string &str);
     LongNum(int n);
     ~LongNum();
 
     size_t size() const;
-    
-    friend std::ostream& operator<<(std::ostream& out, const LongNum& num);
-    int &operator[](int position) ;
 
-    LongNum operator+(const LongNum& second);
-    LongNum operator-(const LongNum& second);
-    LongNum operator*(const LongNum& second);
+    void swap(const LongNum &first, const LongNum &second);
+    LongNum &operator=(const LongNum &other);
+    LongNum &operator=(int &other);
+
+    friend std::ostream &operator<<(std::ostream &out, const LongNum &num);
+    int &operator[](int position);
+
+    LongNum operator+(const LongNum &num);
+    LongNum operator-(const LongNum &num);
+    LongNum operator*(const LongNum &num);
+    LongNum operator/(const LongNum &num);
     LongNum operator-();
-    
-    bool operator==(LongNum& second);
-    bool operator>(LongNum& second);
-    bool operator>=(LongNum& second);
-    bool operator<(LongNum& second);
-    bool operator<=(LongNum& second);
+    LongNum operator+=(const LongNum &num);
+    LongNum operator-=(const LongNum &num);
+
+    bool operator==(LongNum &second);
+    bool operator>(LongNum &second);
+    bool operator>=(LongNum &second);
+    bool operator<(LongNum &second);
+    bool operator<=(LongNum &second);
 };
 
 LongNum::LongNum()
 {
-  sign = true;
-  number.push_back(0);
-} 
+    sign = true;
+    number.push_back(0);
+}
 
-LongNum::LongNum(std::vector<int>& vec, bool sig = true): number(vec), sign(sig) {}
+LongNum::LongNum(std::vector<int> &vec, bool sig = true) : number(vec), sign(sig) {}
 
-
-LongNum::LongNum( std::string & str)
+LongNum::LongNum(std::string &str)
 {
-    sign = str[0]!='-';
+    sign = str[0] != '-';
     if (sign)
     {
-      for (int i=str.size()-1; i>=0; --i)
+        for (int i = str.size() - 1; i >= 0; --i)
         {
             number.push_back(str[i] - '0');
-        }  
+        }
     }
     else
     {
-        for (int i=str.size()-1; i>=1; --i)
+        for (int i = str.size() - 1; i >= 1; --i)
         {
             number.push_back(str[i] - '0');
-        }  
+        }
     }
 }
 
 LongNum::LongNum(int n)
 {
-    sign = n>=0;
-    if (n==0) number.push_back(0);
-    while (n>0)
+    sign = n >= 0;
+    if (n == 0)
+        number.push_back(0);
+    while (n > 0)
     {
-        number.push_back(n%10);
-        n/10;
+        number.push_back(n % 10);
+        n /= 10;
     }
 }
 
@@ -84,170 +90,236 @@ size_t LongNum::size() const
     return number.size();
 }
 
-std::ostream& operator<<(std::ostream& out, const LongNum& num) 
-{   
-    if (!num.sign) out << "-";
-    for (auto it = num.number.rbegin(); it != num.number.rend(); it++) {
+LongNum &LongNum::operator=(const LongNum &other)
+{
+    sign = other.sign;
+    number = other.number;
+    return *this;
+}
+
+LongNum &LongNum::operator=(int &other) // Желательно переделать
+{
+    sign = other >= 0;
+    LongNum tmp{other};
+    *this = tmp;
+    return *this;
+}
+
+std::ostream &operator<<(std::ostream &out, const LongNum &num)
+{
+    if (!num.sign)
+        out << "-";
+    for (auto it = num.number.rbegin(); it != num.number.rend(); it++)
+    {
         out << *it;
     }
     return out;
 }
 
- int &LongNum::operator[](int position) 
-    {
-        return number[position];
-    }
+int &LongNum::operator[](int position)
+{
+    return number[position];
+}
 
-LongNum LongNum::operator+(const LongNum& num)
+LongNum LongNum::operator+(const LongNum &num)
 {
     LongNum first = *this;
     LongNum second = num;
     first.sign = true;
     second.sign = true;
-        if (!sign)
+    if (!sign)
+    {
+        if (!num.sign)
         {
-            if (!num.sign)
-            {
-                LongNum res = first + second;
-                res.sign = false;
-                return res;
-            }
-            else
-            {
-                LongNum res = second - first;
-                return res;
-            } 
+            LongNum res = first + second;
+            res.sign = false;
+            return res;
         }
-        else if (!num.sign)
-            {
-                LongNum res = first - second;
-                return res;       
-            }
-    int c = 0, t=0;
+        else
+        {
+            LongNum res = second - first;
+            return res;
+        }
+    }
+    else if (!num.sign)
+    {
+        LongNum res = first - second;
+        return res;
+    }
+    int c = 0, t = 0;
     if (first >= second)
     {
-       for (size_t i=0; i<=second.size()-1;i++)
-    {
-        t = first[i] + second[i] + c;
-        c = t >= 10;
-        first[i] = t%10;
-    }
-    if (c==1) 
-    {
-        if ((first.size()-second.size())>=1)
+        for (size_t i = 0; i <= second.size() - 1; i++)
         {
-            first.number[second.size()]++;
+            t = first[i] + second[i] + c;
+            c = t >= 10;
+            first[i] = t % 10;
         }
-        else first.number.push_back(1);
-    }
-    return first; 
+        if (c == 1)
+        {
+            if ((first.size() - second.size()) >= 1)
+            {
+                first.number[second.size()]++;
+            }
+            else
+                first.number.push_back(1);
+        }
+        return first;
     }
     return second + first;
 }
 
-
-LongNum LongNum::operator-(const LongNum& num)
+LongNum LongNum::operator-(const LongNum &num)
 {
     LongNum first = *this;
     LongNum second = num;
     first.sign = true;
     second.sign = true;
-        if (!sign)
-        {
-            if (!num.sign)
-            {
-                LongNum res = second - first;
-                return res;
-            }
-            else
-            {
-                LongNum res = first + second;
-                res.sign = false;
-                return res;
-            } 
-        }
-        else if (!num.sign)
-            {
-                LongNum res = first + second;
-                return res;       
-            }
-    int c = 0, t=0;
-    int s = second.size();
-    if (first==second) return LongNum(0);
-    if (first > second)
+    if (!sign)
     {
-       for(size_t i= s; i<first.size();++i)
-       {
-        second.number.push_back(0);
-       }
-       for (size_t i=0; i<=s-1;i++)
-    {
-        t = first[i] - second[i] - c;
-        if (t<0)
+        if (!num.sign)
         {
-         c = 1;  
-         first[i] = t + 10;
+            LongNum res = second - first;
+            return res;
         }
         else
         {
-         c = 0;  
-         first[i] = t;
+            LongNum res = first + second;
+            res.sign = false;
+            return res;
         }
     }
-    if (c==1) first.number.pop_back();
-    while (first.number[first.size()-1] == 0) first.number.pop_back();
-    return first; 
+    else if (!num.sign)
+    {
+        LongNum res = first + second;
+        return res;
+    }
+    int c = 0, t = 0;
+    if (first == second)
+        return LongNum(0);
+    if (first > second)
+    {
+        int s = second.size();
+        for (size_t i = s; i < first.size(); ++i)
+        {
+            second.number.push_back(0);
+        }
+        for (size_t i = 0; i <= first.size() - 1; i++)
+        {
+            t = first[i] - second[i] - c;
+            if (t < 0)
+            {
+                c = 1;
+                first[i] = t + 10;
+            }
+            else
+            {
+                c = 0;
+                first[i] = t;
+            }
+        }
+        /*  if (c==1)
+         {
+             if ((first.size()-s)>=1)
+             {
+                 first.number[s]--;
+             }
+             else first.number.pop_back();
+         }*/
+        while (*first.number.rbegin() == 0)
+            first.number.pop_back();
+        return first;
     }
     LongNum res = second - first;
     res.sign = false;
     return res;
 }
 
-LongNum LongNum::operator*(const LongNum& num) // Дореализовать
+LongNum LongNum::operator*(const LongNum &num)
 {
     LongNum first = *this;
     LongNum second = num;
     first.sign = true;
     second.sign = true;
-        if (!sign)
+
+    if (first > second)
+    {
+        for (size_t i = second.size(); i < first.size(); ++i)
         {
-            if (!num.sign)
-            {
-                LongNum res = first + second;
-                res.sign = false;
-                return res;
-            }
-            else
-            {
-                LongNum res = second - first;
-                return res;
-            } 
+            second.number.push_back(0);
         }
-        else if (!num.sign)
-            {
-                LongNum res = first - second;
-                return res;       
-            }
-    int c = 0, t=0;
-    if (first >= second)
-    {
-       for (size_t i=0; i<=second.size()-1;i++)
-    {
-        t = first[i] + second[i] + c;
-        c = t >= 10;
-        first[i] = t%10;
     }
-    if (c==1) 
+    else
     {
-        if ((first.size()-second.size())>=1)
+        for (size_t i = first.size(); i < second.size(); ++i)
         {
-            first.number[second.size()]++;
+            first.number.push_back(0);
         }
-        else first.number.push_back(1);
     }
-    return first; 
+    int n = first.size();
+    int c = 0, t = 0;
+    std::vector<int> w(n + n, 0);
+    for (int i = 0; i < n; ++i)
+    {
+        c = 0;
+        for (int j = 0; j < n; ++j)
+        {
+            t = w[i + j] + first[j] * second[i] + c;
+            w[i + j] = t % 10;
+            c = t / 10;
+        }
+        w[n + i] = c;
     }
-    return second + first;
+    while (*w.rbegin() == 0)
+        w.pop_back();
+    LongNum res(w);
+    res.sign = !(sign ^ num.sign);
+    return res;
+}
+
+LongNum LongNum::operator/(const LongNum &num) // Предположительно, не закончен
+{
+    LongNum first = *this;
+    LongNum second = num;
+    first.sign = true;
+    second.sign = true;
+    LongNum res;
+    int count = 0;
+    if (first == second)
+    {
+        res = 1;
+        return res;
+    }
+    else if (first < second)
+    {
+        res = 0;
+        return res;
+    }
+    else
+    {
+        first -= second;
+        count++;
+    }
+
+    while (first >= second)
+    {
+        if (first == second)
+        {
+            res = ++count;
+            return res;
+        }
+        else if (first < second)
+        {
+            res = count;
+            return res;
+        }
+        else
+        {
+            first -= second;
+            count++;
+        }
+    }
+    return res;
 }
 
 LongNum LongNum::operator-()
@@ -257,53 +329,80 @@ LongNum LongNum::operator-()
     return res;
 }
 
-bool LongNum::operator==(LongNum& second)
+LongNum LongNum::operator+=(const LongNum &num)
 {
-    if (sign != second.sign) return false;
-    if (number.size() > second.size()) return false;
-    if (number.size() < second.size()) return false;
-    for (int i=0; i<= number.size()-1;++i)
+    *this = *this + num;
+    return *this;
+}
+
+LongNum LongNum::operator-=(const LongNum &num)
+{
+    *this = *this - num;
+    return *this;
+}
+
+bool LongNum::operator==(LongNum &second)
+{
+    if (sign != second.sign)
+        return false;
+    if (number.size() > second.size())
+        return false;
+    if (number.size() < second.size())
+        return false;
+    for (int i = 0; i <= number.size() - 1; ++i)
     {
-        if (number[i]!=second[i]) return false;
+        if (number[i] != second[i])
+            return false;
     }
     return true;
 }
 
- bool LongNum::operator>(LongNum& second)
+bool LongNum::operator>(LongNum &second)
 {
-    if (sign > second.sign) return true;
-    if (sign < second.sign) return false;
-    if (number.size() > second.size()) return sign;
-    if (number.size() < second.size()) return !sign;
+    if (sign > second.sign)
+        return true;
+    if (sign < second.sign)
+        return false;
+    if (number.size() > second.size())
+        return sign;
+    if (number.size() < second.size())
+        return !sign;
     for (int i = number.size() - 1; i >= 0; --i)
     {
-        if (number[i] > second[i]) return sign;
-        if (number[i] < second[i]) return !sign;
+        if (number[i] > second[i])
+            return sign;
+        if (number[i] < second[i])
+            return !sign;
     }
     return false;
 }
 
-
- bool LongNum::operator>=(LongNum& second)
+bool LongNum::operator>=(LongNum &second)
 {
-    return !(*this<second);
+    return !(*this < second);
 }
 
- bool LongNum::operator<(LongNum& second)
+bool LongNum::operator<(LongNum &second)
 {
-    if (sign > second.sign) return false;
-    if (sign < second.sign) return true;
-    if (number.size() < second.size()) return sign;
-    if (number.size() > second.size()) return !sign;
+    if (sign > second.sign)
+        return false;
+    if (sign < second.sign)
+        return true;
+    if (number.size() < second.size())
+        return sign;
+    if (number.size() > second.size())
+        return !sign;
     for (int i = number.size() - 1; i >= 0; --i)
     {
-        if (number[i] < second[i]) return sign;
-        if (number[i] > second[i]) return !sign;
+        if (number[i] < second[i])
+            return sign;
+        if (number[i] > second[i])
+            return !sign;
     }
     return false;
 }
 
- bool LongNum::operator<=(LongNum& second)
+bool LongNum::operator<=(LongNum &second)
 {
-    return !(*this>second);
+    return !(*this > second);
 }
--- main.cpp
+++ main.cpp
@@ -5,17 +5,19 @@ int main()
 {
     long long int a = 911111111111188881;
     long long int b = 919999999998888188;
-   // std::cout << a + b << std::endl;
+    // std::cout << a + b << std::endl;
 
-    std::string k("102345");
-    std::string r("-2335");
+    std::string k("555555");
+    std::string r("44444");
     /*for (int i=0; i < k.number.size(); ++i)
     {
         std:: cout << k.number[i] << " ";
     }*/
     LongNum k1(k);
     LongNum r1(r);
-    std:: cout << k1 - r1 << std::endl;
+    std::cout << k1 - r1 << std::endl;
+    std::cout << k1 + r1 << std::endl;
+    std::cout << -k1 - -r1 << std::endl;
 
     return 0;
 }
